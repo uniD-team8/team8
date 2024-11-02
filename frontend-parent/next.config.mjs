2b976b3bcc8b964662810096b5d3fb890e5d2610
Jimin Ha <jimin9038@g.skku.edu>
2024-11-03 04:21:35 +0900

    chore compile option

--- frontend-parent/next.config.mjs
+++ frontend-parent/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  optimizePackageImports: ["@chakra-ui/react"],
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
